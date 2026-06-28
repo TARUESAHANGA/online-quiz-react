a72cfa25e6275d07edbf83b86883d193d5bd5c4e
TARUESAHANGA <162821252+TARUESAHANGA@users.noreply.github.com>
2026-06-28 12:24:25 +0100

    created navbar for the app

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Sports Quiz</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">Play</a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
