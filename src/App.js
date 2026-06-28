fcedea4d2a9fe30e2f39cbdd06fa626913c739e8
TARUESAHANGA <162821252+TARUESAHANGA@users.noreply.github.com>
2026-06-28 12:49:19 +0100

    added the rules on how to play

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Rules from './components/Rules';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <Rules />
     </div>
   );
 }
---
+++ src/components/Rules.js
@@ -0,0 +1,16 @@
+const Rules = () => {
+  return (
+    <div className="rules-list">
+      <h2>How to play</h2>
+      <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </div>
+  );
+}
+ 
+export default Rules;
